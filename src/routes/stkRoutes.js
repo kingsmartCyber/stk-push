6e15d5b1c382d0cba41515a37c2eb8911b629837
Kingsmart Cyber <162454101+kingsmartCyber@users.noreply.github.com>
2024-09-01 22:22:29 +0300

    Delete src directory

--- src/routes/stkRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { initiateStkPush } from '../controllers/stkController.js';
-
-const router = express.Router();
-
-router.post('/push', initiateStkPush);
-
-export default router;
